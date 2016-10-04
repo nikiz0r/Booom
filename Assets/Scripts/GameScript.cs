d0179cf60d3b981ecb0716f1fce23c4e30611d2a
Rumenigue Nogueira da Silva <niguerag@hotmail.com>
2016-10-04 16:56:28 -0300

    setup

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        var res = Resources.LoadAll("Map");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private float baseMoveSpeed = 3;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        var xDirection = Input.GetAxisRaw("Horizontal") * baseMoveSpeed;
+        var yDirection = Input.GetAxisRaw("Vertical") * baseMoveSpeed;
+
+        rb.velocity = new Vector2(xDirection, yDirection);
+    }
+}
